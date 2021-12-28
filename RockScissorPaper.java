ad79983e6580fefd9f05932d8d7a4d65e309426d
SatelliteSilver <wse0118@naver.com>
2021-12-28 23:59:31 +0900

    add method write()

--- RockScissorPaper.java
+++ RockScissorPaper.java
@@ -1,6 +1,19 @@
+import java.io.*;
 public class RockScissorPaper {
 	  RockScissorPaper(){}
 
+	  void write()
+	  {
+		File file = new File("log.txt");
+		try {
+		    FileWriter fileWriter = new FileWriter(file);
+		    PrintWriter printWriter = new PrintWriter(fileWriter);
+		    printWriter.print(play());
+		    printWriter.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	  }
 	  public static void main(String args[])
 	  {
 	        RockScissorPaper game = new RockScissorPaper();
