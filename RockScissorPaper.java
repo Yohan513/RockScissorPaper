2e4e780a6885716b20b927167fe5c644a492e837
Yohan513 <zeus5137@gmail.com>
2021-12-29 00:51:35 +0900

    play method

--- RockScissorPaper.java
+++ RockScissorPaper.java
@@ -12,4 +12,46 @@ public class RockScissorPaper {
 
 	        }
 	  }
+
+	  public static String play() throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String U = bf.readLine();
+		String result = "";
+		//String[] Com = {"Rock", "Scissor", "Paper"};
+		Random rand = new Random();
+		int ranNum = rand.nextInt(2);
+		while(true) {
+			if(U.equals("Rock")) {
+				switch(ranNum) {
+					case 0:
+						result = "무";
+					case 1:
+						result = "승";
+					case 2:
+						result = "패";
+				}
+			} else if(U.equals("Scissor")) {
+				switch(ranNum) {
+					case 0:
+						result = "패";
+					case 1:
+						result = "무";
+					case 2:
+						result = "승";
+				} 
+			} else {
+				switch(ranNum) {
+					case 0:
+						result = "승";
+					case 1:
+						result = "패";
+					case 2:
+						result = "무";
+				}
+			}
+			return result;
+
+		}
+		
+	}
 }
