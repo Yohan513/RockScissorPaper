5df7ae9be61cbbe58b4d4e3386d6b56c4cb029f4
ParkSeungbeom <sbpark0630@khu.ac.kr>
2021-12-29 00:28:32 +0900

    [Add] TextFileLoad Method

--- RockScissorPaper.java
+++ RockScissorPaper.java
@@ -1,13 +1,41 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 public class RockScissorPaper {
 	  RockScissorPaper(){}
+	  
+	  public static void TextFileLoad() throws IOException {
+		  FileReader reader = new FileReader("log.txt", Charset.forName("UTF-8"));
+		  
+		  int ch;
+		  char result;
+		  int winCount = 0, drawCount = 0, loseCount = 0, gameCount = 0;
+		  while((ch = reader.read()) != -1) {
+			  result = (char) ch;
+			  if(result == '½Â') {
+				  winCount += 1;
+			  } else if(result == '¹«') {
+				  drawCount += 1;
+			  } else if(result == 'ÆÐ') {
+				  loseCount += 1;
+			  }
+			  gameCount += 1;
+		  }
+		  System.out.println(gameCount + "Àü" + winCount + "½Â" + drawCount + "¹«" + loseCount + "ÆÐ" + " ½Â·ü" + Math.round(((float) winCount / gameCount * 100)) + "%");
+	  }
 
-	  public static void main(String args[])
+	  public static void main(String args[]) throws IOException
 	  {
 	        RockScissorPaper game = new RockScissorPaper();
+	        String str = "Hello!";
 
 	        // game start
 	        while(true){
-
+	        	TextFileLoad();
+	        	if(str == "Hello!") {
+	        		break;
+	        	}
 
 
 	        }
